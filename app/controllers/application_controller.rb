ceb3d35c506889c20f1bf63f5a7ff6308d3733bf
jeffherbst <jeffreyherbst18@gmail.com>
2018-09-14 09:21:30 -0700

    added views to show games and list games with links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,24 @@ class ApplicationController < ActionController::Base
     name = params['name'] || "World"
     render 'application/hello_world', locals: {name: name}
   end
+
+  def list_games
+    games = connection.execute("SELECT * FROM games")
+
+    render 'application/list_games', locals: {games: games}
+  end
+
+  def show_game
+    game = connection.execute("SELECT * FROM games WHERE games.id = ? LIMIT 1", params['id']).first
+
+    render 'application/show_game', locals: {game: game}
+  end
+
+  private
+
+  def connection
+    db_connection = SQLite3::Database.new 'db/development.sqlite3'
+    db_connection.results_as_hash = true
+    db_connection
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/hello_world' => 'application#hello_world'
   get '/hello/:name' => 'application#hello_world'
+  get '/list_games' => 'application#list_games'
+  get '/show_game/:id' => 'application#show_game'
 end
